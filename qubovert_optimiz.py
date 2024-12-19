86446c6c21448eae00c1bc243e070c4987c6bfe9
MakarMariia97 <mariamaltseva1997@gmail.com>
2024-12-19 11:46:03 +0300

    Update qubovert_optimiz.py

--- qubovert_optimiz.py
+++ qubovert_optimiz.py
@@ -4,7 +4,7 @@ import itertools
 from qubovert.sat import OR, NOT
 from qubovert import PCBO
 import numpy as np
-from utils import manhattan, astar_search, annealer
+from utils_qubovert import manhattan, astar_search, annealer
 
 # function to consruct boolean function in CNF for the circuit
 def check_sat(s,nvars,ngates,nrows,ncols,x, sim):
@@ -24,7 +24,7 @@ def check_sat(s,nvars,ngates,nrows,ncols,x, sim):
     for g in range(ngates):
         for c in range(len(grid)):
             cnf1.add_constraint_OR(NOT(x[grid[c][0],grid[c][1],s[g][0]]),OR(*[x[c1[0],c1[1],s[g][1]] for c1 in (y for y in grid if manhattan(grid[c],y)==1)]),lam=100)
-
+            # these two OR for 3-qubit gates
     ifsat = False
     N=1 # number of repetitions (needed because of probabilistic nature of Quantum Annealing)
     indN=0
@@ -43,39 +43,89 @@ def check_sat(s,nvars,ngates,nrows,ncols,x, sim):
 
     return ifsat, solution
 
-sim = False # simulation mode, if True
+pr = input("Which quantum circuit would you like to optimize (Toffoli, double Toffoli, Fredkin, 2-4 decoder or CNOT-based)? Press the corresponding index (starting with 1).")
+if pr == "1":
+    toffoli = True
+    dToffoli = False
+    fredkin = False
+    decod = False
+    cnotbased = False
+elif pr == "2":
+    toffoli = False
+    dToffoli = True
+    fredkin = False
+    decod = False
+    cnotbased = False
+elif pr == "3":
+    toffoli = False
+    dToffoli = False
+    fredkin = True
+    decod = False
+    cnotbased = False
+elif pr == "4":
+    toffoli = False
+    dToffoli = False
+    fredkin = False
+    decod = True
+    cnotbased = False
+elif pr == "5":
+    toffoli = False
+    dToffoli = False
+    fredkin = False
+    decod = False
+    cnotbased = True
+else:
+        print("[ERROR] string is not valid, exiting...")
+        exit(2)
 
-# CNOT-based circuit
-# gates=[[0,2],[1,3],[3,4],[1,4],[0,3],[0,2]]
-# dep_graph={2:[1], 4:[2]}  # gate dependecies meaning that the 2nd gate in 'gates' list depends on the 1st one (starting with 0 index), etc.
-# nvars = 5
+if toffoli:
+    # Toffoli gate
+    gates=[[2,0],[1,0],[2,1],[1,0],[2,1]]
+    dep_graph={2:[1]} # gate dependecies meaning that the 2nd gate in 'gates' list depends on the 1st one (starting with 0 index), etc.
+    nvars = 3
+elif dToffoli:
+    # double Toffoli gate
+    gates = [[2,1],[1,0],[3,0],[1,0],[3,0],[1,3],[2,1]]
+    dep_graph = {1:[0], 2:[1], 3:[2], 4:[3], 5:[0], 6:[3,5]}
+    nvars = 4
+elif fredkin:
+    # Fredkin gate
+    gates = [[2,1],[0,2],[1,2],[0,1],[1,2],[2,1],[0,1]]
+    dep_graph = {1:[0], 2:[0], 3:[2], 4:[3], 5:[1,4], 6:[5]}
+    nvars = 3
+elif decod:
+    # 2-4 decoder
+    gates=[[2,1],[3,1],[3,0],[0,2],[2,1],[1,2],[2,0],[1,3]]
+    dep_graph={3:[0,2], 4:[3], 5:[4], 6:[5], 7:[1,2,4]}
+    nvars = 4
+else:
+    # CNOT-based circuit
+    gates=[[0,2],[1,3],[3,4],[1,4],[0,3],[0,2]]
+    dep_graph={2:[1], 4:[2]}  # gate dependecies meaning that the 2nd gate in 'gates' list depends on the 1st one (starting with 0 index), etc.
+    nvars = 5
 
-# double Toffoli gate
-# gates = [[2,1],[1,0],[3,0],[1,0],[3,0],[1,3],[2,1]]
-# dep_graph = {1:[0], 2:[1], 3:[2], 4:[3], 5:[0], 6:[3,5]}
-# nvars = 4
+pr1 = input("Which mode would you like to run: Quantum Annealing or Simulated Annealing? Press 1 for Quantum Annealing and 0 otherwise.")
+if pr1 == "1":
+    sim=False
+elif pr1 == "0":
+    sim=True
+else:
+    print("[ERROR] string is not valid, exiting...")
+    exit(2)
 
-# Fredkin gate
-# gates = [[2,1],[0,2],[1,2],[0,1],[1,2],[2,1],[0,1]]
-# dep_graph = {1:[0], 2:[0], 3:[2], 4:[3], 5:[1,4], 6:[5]}
-# nvars = 3
+pr2 = input("Which dimension of architecture would you like to optimize the circuit for: linear or 2-dimensional? Press 1 for linear and 2 otherwise.")
+if pr2 == "1":
+    nrows=1
+    ncols=nvars
+elif pr2 == "2":
+    nrows=2
+    ncols=int(np.ceil(nvars/2))
 
-# 2-4 decoder
-# gates=[[2,1],[3,1],[3,0],[0,2],[2,1],[1,2],[2,0],[1,3]]
-# dep_graph={3:[0,2], 4:[3], 5:[4], 6:[5], 7:[1,2,4]}
-# nvars = 4
- 
-# Toffoli gate
-gates=[[2,0],[1,0],[2,1],[1,0],[2,1]]
-dep_graph={2:[1], 3:[2], 4:[3]}
-nvars = 3
+
 
 ngates_all = len(gates)
 
-
 # grid
-nrows=2
-ncols=2
 grid = [(i,j) for i in range(nrows) for j in range(ncols)]
 
 nsubcirc=0 # initial number of subcircuits
@@ -171,7 +221,7 @@ while free_gates:
         nsubcirc+=1
 
 # convert characteristic vectors X (denoting qubit position) obtained for each optimal subcircuit into grid
-placements = [[[-1 for x in range(ncols)] for x in range(nrows)] for p in range(len(optimal_placements))]
+placements = [[[-1 for x in range(ncols)] for x in range(nrows)] for p in range(len(optimal_placements))] # omit excess placements
 for p in range(len(optimal_placements)):
     for (i,j) in grid:
         for (key,val) in optimal_placements[p].items():
@@ -189,4 +239,3 @@ for p in range(len(placements)):
     print("subcircuit:",optimal_s[p])
     print(placements[p])
 print("SWAP count:",swap_count)
-
